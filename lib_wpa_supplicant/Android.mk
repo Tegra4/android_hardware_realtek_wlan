cf6ae81da689cf533dbc2f53c55fbac24dd8f77c
Tiberiu Breana <tiberiu.a.breana@intel.com>
2014-08-19 12:45:49 +0300

    Revert "realtek: wpa_supplicant_lib: Multi-lib support for wpa_supplicant"
    This reverts commit aa48fe99646f9a5e25518194dc88e65fd7aabbac.
    The purpose of this revert is to keep the standard function names
    used in aosp.
    
    Issue: GMINL-222
    Change-Id: I9caecf578394817dff41e128b3369e76c624a691
    Signed-off-by: Tiberiu Breana <tiberiu.a.breana@intel.com>

--- lib_wpa_supplicant/Android.mk
+++ lib_wpa_supplicant/Android.mk
@@ -15,7 +15,7 @@
 #
 LOCAL_PATH := $(call my-dir)
 
-#ifeq ($(WPA_SUPPLICANT_VERSION),VER_0_8_X)
+ifeq ($(WPA_SUPPLICANT_VERSION),VER_0_8_X)
 
 ifneq ($(BOARD_WPA_SUPPLICANT_DRIVER),)
   CONFIG_DRIVER_$(BOARD_WPA_SUPPLICANT_DRIVER) := y
@@ -56,7 +56,6 @@ endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := lib_driver_cmd_rtl
-LOCAL_MODULE_TAGS := optional
 LOCAL_SHARED_LIBRARIES := libc libcutils
 LOCAL_CFLAGS := $(L_CFLAGS)
 LOCAL_SRC_FILES := $(WPA_SRC_FILE)
@@ -65,4 +64,4 @@ include $(BUILD_STATIC_LIBRARY)
 
 ########################
 
-#endif
+endif
