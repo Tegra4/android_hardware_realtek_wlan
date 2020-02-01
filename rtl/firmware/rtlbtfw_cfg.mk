431703a669a2168e495926a9185d871e172f2438
C457 <android.c357@gmail.com>
2020-01-31 22:26:22 -0600

    wlan: Update Wifi/BT firmware
    TODO: Add all firmware size is relatively small

--- rtl/firmware/rtlbtfw_cfg.mk
+++ rtl/firmware/rtlbtfw_cfg.mk
@@ -20,18 +20,6 @@ LOCAL_PATH := hardware/realtek/wlan/rtl/firmware
 -include hardware/realtek/wlan/rtl/config/config-rtl.mk
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rtl8703b_config:system/etc/firmware/rtl8703bs_config \
-    $(LOCAL_PATH)/rtl8703b_fw:system/etc/firmware/rtl8703bs_fw \
-    $(LOCAL_PATH)/rtl8723a_config:system/etc/firmware/rtl8723as_config \
-    $(LOCAL_PATH)/rtl8723a_fw:system/etc/firmware/rtl8723as_fw \
-    $(LOCAL_PATH)/rtl8723b_config:system/etc/firmware/rtl8723bs_config \
-    $(LOCAL_PATH)/rtl8723b_fw:system/etc/firmware/rtl8723bs_fw \
-    $(LOCAL_PATH)/rtl8723b_VQ0_config:system/etc/firmware/rtl8723bs_VQ0_config \
-    $(LOCAL_PATH)/rtl8723b_VQ0_fw:system/etc/firmware/rtl8723bs_VQ0_fw \
-    $(LOCAL_PATH)/rtl8723cs_cg_config:system/etc/firmware/rtl8723cs_cg_config \
-    $(LOCAL_PATH)/rtl8723cs_cg_fw:system/etc/firmware/rtl8723cs_cg_fw \
-    $(LOCAL_PATH)/rtl8723cs_vf_config:system/etc/firmware/rtl8723cs_vf_config \
-    $(LOCAL_PATH)/rtl8723cs_vf_fw:system/etc/firmware/rtl8723cs_vf_fw \
-    $(LOCAL_PATH)/rtl8723cs_xx_config:system/etc/firmware/rtl8723cs_xx_config \
-    $(LOCAL_PATH)/rtl8723cs_xx_fw:system/etc/firmware/rtl8723cs_xx_fw
+    $(LOCAL_PATH)/rtl8723b_config:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/rtl8723bs_config \
+    $(LOCAL_PATH)/rtl8723b_fw:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/rtl8723bs_fw
 
